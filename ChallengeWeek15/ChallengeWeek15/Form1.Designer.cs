34209fac5579cb7d061835625d612d5da012bc1f
Christopher tekvi <christophertekvi@hotmail.com>
2021-01-21 23:54:10 +0700

    Challenge Week 15

---
+++ ChallengeWeek15/ChallengeWeek15/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿
+namespace ChallengeWeek15
+{
+    partial class FormKalkulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMC = new System.Windows.Forms.Button();
+            this.buttonLima = new System.Windows.Forms.Button();
+            this.buttonDelapan = new System.Windows.Forms.Button();
+            this.buttonCE = new System.Windows.Forms.Button();
+            this.buttonSatu = new System.Windows.Forms.Button();
+            this.buttonEmpat = new System.Windows.Forms.Button();
+            this.buttonTujuh = new System.Windows.Forms.Button();
+            this.buttonHapus = new System.Windows.Forms.Button();
+            this.buttonMPlus = new System.Windows.Forms.Button();
+            this.buttonMS = new System.Windows.Forms.Button();
+            this.buttonMR = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonPlusMin = new System.Windows.Forms.Button();
+            this.buttonSembilan = new System.Windows.Forms.Button();
+            this.buttonEnam = new System.Windows.Forms.Button();
+            this.buttonBagi = new System.Windows.Forms.Button();
+            this.buttonDua = new System.Windows.Forms.Button();
+            this.buttonTiga = new System.Windows.Forms.Button();
+            this.buttonKali = new System.Windows.Forms.Button();
+            this.buttonKurang = new System.Windows.Forms.Button();
+            this.buttonHasil = new System.Windows.Forms.Button();
+            this.buttonPecahan = new System.Windows.Forms.Button();
+            this.buttonPersen = new System.Windows.Forms.Button();
+            this.buttonMNega = new System.Windows.Forms.Button();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.buttonKoma = new System.Windows.Forms.Button();
+            this.buttonNol = new System.Windows.Forms.Button();
+            this.buttonAkar = new System.Windows.Forms.Button();
+            this.listViewTampilan = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonMC
+            // 
+            this.buttonMC.Location = new System.Drawing.Point(12, 90);
+            this.buttonMC.Name = "buttonMC";
+            this.buttonMC.Size = new System.Drawing.Size(40, 40);
+            this.buttonMC.TabIndex = 0;
+            this.buttonMC.Text = "MC";
+            this.buttonMC.UseVisualStyleBackColor = true;
+            // 
+            // buttonLima
+            // 
+            this.buttonLima.Location = new System.Drawing.Point(58, 228);
+            this.buttonLima.Name = "buttonLima";
+            this.buttonLima.Size = new System.Drawing.Size(40, 40);
+            this.buttonLima.TabIndex = 1;
+            this.buttonLima.Text = "5";
+            this.buttonLima.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelapan
+            // 
+            this.buttonDelapan.Location = new System.Drawing.Point(58, 182);
+            this.buttonDelapan.Name = "buttonDelapan";
+            this.buttonDelapan.Size = new System.Drawing.Size(40, 40);
+            this.buttonDelapan.TabIndex = 2;
+            this.buttonDelapan.Text = "8";
+            this.buttonDelapan.UseVisualStyleBackColor = true;
+            // 
+            // buttonCE
+            // 
+            this.buttonCE.Location = new System.Drawing.Point(58, 136);
+            this.buttonCE.Name = "buttonCE";
+            this.buttonCE.Size = new System.Drawing.Size(40, 40);
+            this.buttonCE.TabIndex = 3;
+            this.buttonCE.Text = "CE";
+            this.buttonCE.UseVisualStyleBackColor = true;
+            // 
+            // buttonSatu
+            // 
+            this.buttonSatu.Location = new System.Drawing.Point(12, 274);
+            this.buttonSatu.Name = "buttonSatu";
+            this.buttonSatu.Size = new System.Drawing.Size(40, 40);
+            this.buttonSatu.TabIndex = 4;
+            this.buttonSatu.Text = "1";
+            this.buttonSatu.UseVisualStyleBackColor = true;
+            this.buttonSatu.Click += new System.EventHandler(this.buttonSatu_Click);
+            // 
+            // buttonEmpat
+            // 
+            this.buttonEmpat.Location = new System.Drawing.Point(12, 228);
+            this.buttonEmpat.Name = "buttonEmpat";
+            this.buttonEmpat.Size = new System.Drawing.Size(40, 40);
+            this.buttonEmpat.TabIndex = 5;
+            this.buttonEmpat.Text = "4";
+            this.buttonEmpat.UseVisualStyleBackColor = true;
+            // 
+            // buttonTujuh
+            // 
+            this.buttonTujuh.Location = new System.Drawing.Point(12, 182);
+            this.buttonTujuh.Name = "buttonTujuh";
+            this.buttonTujuh.Size = new System.Drawing.Size(40, 40);
+            this.buttonTujuh.TabIndex = 6;
+            this.buttonTujuh.Text = "7";
+            this.buttonTujuh.UseVisualStyleBackColor = true;
+            // 
+            // buttonHapus
+            // 
+            this.buttonHapus.Location = new System.Drawing.Point(12, 136);
+            this.buttonHapus.Name = "buttonHapus";
+            this.buttonHapus.Size = new System.Drawing.Size(40, 40);
+            this.buttonHapus.TabIndex = 7;
+            this.buttonHapus.Text = "←";
+            this.buttonHapus.UseVisualStyleBackColor = true;
+            // 
+            // buttonMPlus
+            // 
+            this.buttonMPlus.Location = new System.Drawing.Point(148, 90);
+            this.buttonMPlus.Name = "buttonMPlus";
+            this.buttonMPlus.Size = new System.Drawing.Size(40, 40);
+            this.buttonMPlus.TabIndex = 8;
+            this.buttonMPlus.Text = "M+";
+            this.buttonMPlus.UseVisualStyleBackColor = true;
+            // 
+            // buttonMS
+            // 
+            this.buttonMS.Location = new System.Drawing.Point(104, 90);
+            this.buttonMS.Name = "buttonMS";
+            this.buttonMS.Size = new System.Drawing.Size(40, 40);
+            this.buttonMS.TabIndex = 9;
+            this.buttonMS.Text = "MS";
+            this.buttonMS.UseVisualStyleBackColor = true;
+            // 
+            // buttonMR
+            // 
+            this.buttonMR.Location = new System.Drawing.Point(58, 90);
+            this.buttonMR.Name = "buttonMR";
+            this.buttonMR.Size = new System.Drawing.Size(40, 40);
+            this.buttonMR.TabIndex = 10;
+            this.buttonMR.Text = "MR";
+            this.buttonMR.UseVisualStyleBackColor = true;
+            // 
+            // buttonC
+            // 
+            this.buttonC.Location = new System.Drawing.Point(104, 136);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(40, 40);
+            this.buttonC.TabIndex = 11;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlusMin
+            // 
+            this.buttonPlusMin.Location = new System.Drawing.Point(148, 136);
+            this.buttonPlusMin.Name = "buttonPlusMin";
+            this.buttonPlusMin.Size = new System.Drawing.Size(40, 40);
+            this.buttonPlusMin.TabIndex = 12;
+            this.buttonPlusMin.Text = "±";
+            this.buttonPlusMin.UseVisualStyleBackColor = true;
+            // 
+            // buttonSembilan
+            // 
+            this.buttonSembilan.Location = new System.Drawing.Point(104, 182);
+            this.buttonSembilan.Name = "buttonSembilan";
+            this.buttonSembilan.Size = new System.Drawing.Size(40, 40);
+            this.buttonSembilan.TabIndex = 13;
+            this.buttonSembilan.Text = "9";
+            this.buttonSembilan.UseVisualStyleBackColor = true;
+            // 
+            // buttonEnam
+            // 
+            this.buttonEnam.Location = new System.Drawing.Point(104, 228);
+            this.buttonEnam.Name = "buttonEnam";
+            this.buttonEnam.Size = new System.Drawing.Size(40, 40);
+            this.buttonEnam.TabIndex = 14;
+            this.buttonEnam.Text = "6";
+            this.buttonEnam.UseVisualStyleBackColor = true;
+            // 
+            // buttonBagi
+            // 
+            this.buttonBagi.Location = new System.Drawing.Point(148, 182);
+            this.buttonBagi.Name = "buttonBagi";
+            this.buttonBagi.Size = new System.Drawing.Size(40, 40);
+            this.buttonBagi.TabIndex = 15;
+            this.buttonBagi.Text = "/";
+            this.buttonBagi.UseVisualStyleBackColor = true;
+            // 
+            // buttonDua
+            // 
+            this.buttonDua.Location = new System.Drawing.Point(58, 274);
+            this.buttonDua.Name = "buttonDua";
+            this.buttonDua.Size = new System.Drawing.Size(40, 40);
+            this.buttonDua.TabIndex = 16;
+            this.buttonDua.Text = "2";
+            this.buttonDua.UseVisualStyleBackColor = true;
+            // 
+            // buttonTiga
+            // 
+            this.buttonTiga.Location = new System.Drawing.Point(104, 274);
+            this.buttonTiga.Name = "buttonTiga";
+            this.buttonTiga.Size = new System.Drawing.Size(40, 40);
+            this.buttonTiga.TabIndex = 17;
+            this.buttonTiga.Text = "3";
+            this.buttonTiga.UseVisualStyleBackColor = true;
+            // 
+            // buttonKali
+            // 
+            this.buttonKali.Location = new System.Drawing.Point(148, 228);
+            this.buttonKali.Name = "buttonKali";
+            this.buttonKali.Size = new System.Drawing.Size(40, 40);
+            this.buttonKali.TabIndex = 18;
+            this.buttonKali.Text = "*";
+            this.buttonKali.UseVisualStyleBackColor = true;
+            // 
+            // buttonKurang
+            // 
+            this.buttonKurang.Location = new System.Drawing.Point(148, 274);
+            this.buttonKurang.Name = "buttonKurang";
+            this.buttonKurang.Size = new System.Drawing.Size(40, 40);
+            this.buttonKurang.TabIndex = 19;
+            this.buttonKurang.Text = "-";
+            this.buttonKurang.UseVisualStyleBackColor = true;
+            // 
+            // buttonHasil
+            // 
+            this.buttonHasil.Location = new System.Drawing.Point(194, 274);
+            this.buttonHasil.Name = "buttonHasil";
+            this.buttonHasil.Size = new System.Drawing.Size(40, 86);
+            this.buttonHasil.TabIndex = 24;
+            this.buttonHasil.Text = "=";
+            this.buttonHasil.UseVisualStyleBackColor = true;
+            // 
+            // buttonPecahan
+            // 
+            this.buttonPecahan.Location = new System.Drawing.Point(194, 228);
+            this.buttonPecahan.Name = "buttonPecahan";
+            this.buttonPecahan.Size = new System.Drawing.Size(40, 40);
+            this.buttonPecahan.TabIndex = 22;
+            this.buttonPecahan.Text = "1/x";
+            this.buttonPecahan.UseVisualStyleBackColor = true;
+            // 
+            // buttonPersen
+            // 
+            this.buttonPersen.Location = new System.Drawing.Point(194, 182);
+            this.buttonPersen.Name = "buttonPersen";
+            this.buttonPersen.Size = new System.Drawing.Size(40, 40);
+            this.buttonPersen.TabIndex = 21;
+            this.buttonPersen.Text = "%";
+            this.buttonPersen.UseVisualStyleBackColor = true;
+            // 
+            // buttonMNega
+            // 
+            this.buttonMNega.Location = new System.Drawing.Point(194, 90);
+            this.buttonMNega.Name = "buttonMNega";
+            this.buttonMNega.Size = new System.Drawing.Size(40, 40);
+            this.buttonMNega.TabIndex = 20;
+            this.buttonMNega.Text = "M-";
+            this.buttonMNega.UseVisualStyleBackColor = true;
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Location = new System.Drawing.Point(148, 320);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(40, 40);
+            this.buttonTambah.TabIndex = 28;
+            this.buttonTambah.Text = "+";
+            this.buttonTambah.UseVisualStyleBackColor = true;
+            // 
+            // buttonKoma
+            // 
+            this.buttonKoma.Location = new System.Drawing.Point(104, 320);
+            this.buttonKoma.Name = "buttonKoma";
+            this.buttonKoma.Size = new System.Drawing.Size(40, 40);
+            this.buttonKoma.TabIndex = 27;
+            this.buttonKoma.Text = ".";
+            this.buttonKoma.UseVisualStyleBackColor = true;
+            // 
+            // buttonNol
+            // 
+            this.buttonNol.Location = new System.Drawing.Point(12, 320);
+            this.buttonNol.Name = "buttonNol";
+            this.buttonNol.Size = new System.Drawing.Size(86, 40);
+            this.buttonNol.TabIndex = 25;
+            this.buttonNol.Text = "0";
+            this.buttonNol.UseVisualStyleBackColor = true;
+            // 
+            // buttonAkar
+            // 
+            this.buttonAkar.Location = new System.Drawing.Point(194, 136);
+            this.buttonAkar.Name = "buttonAkar";
+            this.buttonAkar.Size = new System.Drawing.Size(40, 40);
+            this.buttonAkar.TabIndex = 29;
+            this.buttonAkar.Text = "√";
+            this.buttonAkar.UseVisualStyleBackColor = true;
+            // 
+            // listViewTampilan
+            // 
+            this.listViewTampilan.HideSelection = false;
+            this.listViewTampilan.Location = new System.Drawing.Point(12, 12);
+            this.listViewTampilan.Name = "listViewTampilan";
+            this.listViewTampilan.Size = new System.Drawing.Size(222, 63);
+            this.listViewTampilan.TabIndex = 30;
+            this.listViewTampilan.UseCompatibleStateImageBehavior = false;
+            // 
+            // FormKalkulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 372);
+            this.Controls.Add(this.listViewTampilan);
+            this.Controls.Add(this.buttonAkar);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.buttonKoma);
+            this.Controls.Add(this.buttonNol);
+            this.Controls.Add(this.buttonHasil);
+            this.Controls.Add(this.buttonPecahan);
+            this.Controls.Add(this.buttonPersen);
+            this.Controls.Add(this.buttonMNega);
+            this.Controls.Add(this.buttonKurang);
+            this.Controls.Add(this.buttonKali);
+            this.Controls.Add(this.buttonTiga);
+            this.Controls.Add(this.buttonDua);
+            this.Controls.Add(this.buttonBagi);
+            this.Controls.Add(this.buttonEnam);
+            this.Controls.Add(this.buttonSembilan);
+            this.Controls.Add(this.buttonPlusMin);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonMR);
+            this.Controls.Add(this.buttonMS);
+            this.Controls.Add(this.buttonMPlus);
+            this.Controls.Add(this.buttonHapus);
+            this.Controls.Add(this.buttonTujuh);
+            this.Controls.Add(this.buttonEmpat);
+            this.Controls.Add(this.buttonSatu);
+            this.Controls.Add(this.buttonCE);
+            this.Controls.Add(this.buttonDelapan);
+            this.Controls.Add(this.buttonLima);
+            this.Controls.Add(this.buttonMC);
+            this.Name = "FormKalkulator";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMC;
+        private System.Windows.Forms.Button buttonLima;
+        private System.Windows.Forms.Button buttonDelapan;
+        private System.Windows.Forms.Button buttonCE;
+        private System.Windows.Forms.Button buttonSatu;
+        private System.Windows.Forms.Button buttonEmpat;
+        private System.Windows.Forms.Button buttonTujuh;
+        private System.Windows.Forms.Button buttonHapus;
+        private System.Windows.Forms.Button buttonMPlus;
+        private System.Windows.Forms.Button buttonMS;
+        private System.Windows.Forms.Button buttonMR;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonPlusMin;
+        private System.Windows.Forms.Button buttonSembilan;
+        private System.Windows.Forms.Button buttonEnam;
+        private System.Windows.Forms.Button buttonBagi;
+        private System.Windows.Forms.Button buttonDua;
+        private System.Windows.Forms.Button buttonTiga;
+        private System.Windows.Forms.Button buttonKali;
+        private System.Windows.Forms.Button buttonKurang;
+        private System.Windows.Forms.Button buttonHasil;
+        private System.Windows.Forms.Button buttonPecahan;
+        private System.Windows.Forms.Button buttonPersen;
+        private System.Windows.Forms.Button buttonMNega;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.Button buttonKoma;
+        private System.Windows.Forms.Button buttonNol;
+        private System.Windows.Forms.Button buttonAkar;
+        private System.Windows.Forms.ListView listViewTampilan;
+    }
+}
+
---
+++ ChallengeWeek15/ChallengeWeek15/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChallengeWeek15
+{
+    public partial class FormKalkulator : Form
+    {
+        public FormKalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSatu_Click(object sender, EventArgs e)
+        {
+            listViewTampilan.Items.Add("1");
+        }
+    }
+}
